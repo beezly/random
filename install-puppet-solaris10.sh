e2c40b6659c4ebcbb972b07cfdbd6989fef7b793
Andrew Beresford <a.j.beresford@sheffield.ac.uk>
2011-08-05 09:35:15 +0100

    install puppet on solaris 10 script updated README

---
+++ install-puppet-solaris10.sh
@@ -0,0 +1,6 @@
+yes | pkgadd -G -d http://mirrors.shef.ac.uk/opencsw/pkgutil-`uname -p`.pkg all
+mv /opt/csw/etc/pkgutil.conf /opt/csw/etc/pkgutil.conf.orig
+sed -e 's/^#pkgaddopts=-G -S$/pkgaddopts=-G/g' < /opt/csw/etc/pkgutil.conf.orig > /opt/csw/etc/pkgutil.conf.inter
+sed -e 's/^#mirror=.*/mirror=http:\/\/mirrors.shef.ac.uk\/opencsw\/current/g' < /opt/csw/etc/pkgutil.conf.inter > /opt/csw/etc/pkgutil.conf
+rm -f /opt/csw/etc/pkgutil.conf.orig /opt/csw/etc/pkgutil.conf.inter
+/opt/csw/bin/pkgutil -y -i puppet
