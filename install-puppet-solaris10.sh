704ab5ff503a67916c81eab426a1ae9134107566
Andrew Beresford <beezly@beez.ly>
2013-01-07 13:27:30 +0000

    Update install-puppet-solaris10.sh
    Use puppet3 instead of puppet

--- install-puppet-solaris10.sh
+++ install-puppet-solaris10.sh
@@ -3,4 +3,4 @@ mv /opt/csw/etc/pkgutil.conf /opt/csw/etc/pkgutil.conf.orig
 sed -e 's/^#pkgaddopts=-G -S$/pkgaddopts=-G/g' < /opt/csw/etc/pkgutil.conf.orig > /opt/csw/etc/pkgutil.conf.inter
 sed -e 's/^#mirror=.*/mirror=http:\/\/mirrors.shef.ac.uk\/opencsw\/current/g' < /opt/csw/etc/pkgutil.conf.inter > /opt/csw/etc/pkgutil.conf
 rm -f /opt/csw/etc/pkgutil.conf.orig /opt/csw/etc/pkgutil.conf.inter
-/opt/csw/bin/pkgutil -y -i puppet
+/opt/csw/bin/pkgutil -y -i puppet3
